3a91a801057c8d03e4d00b6fced1503df11797c2
Christos Chronis <34183674+chronis10@users.noreply.github.com>
2019-02-12 16:22:20 +0200

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,15 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package domes;
 
 import java.util.ArrayList;
 
 /**
  *
- * @author hroni
+ * @author Christos Chronis
  */
 public class Node {
     private double posx = 0;
